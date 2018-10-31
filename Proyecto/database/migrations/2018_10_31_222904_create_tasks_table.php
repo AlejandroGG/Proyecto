24337fdde75897af5171cbe160be8f4dbc6a72a3
AGG <agalli2010@hotmail.com>
2018-10-31 18:35:43 -0400

    php artisan migrate y se pueblan las variables en mysql

--- Proyecto/database/migrations/2018_10_31_222904_create_tasks_table.php
+++ Proyecto/database/migrations/2018_10_31_222904_create_tasks_table.php
@@ -15,6 +15,9 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+
+              $table->text ('body'); 
+
             $table->timestamps();
         });
     }
